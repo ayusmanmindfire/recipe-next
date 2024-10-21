e1bf7d53845433ab9ec264bd178144502b597273
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2024-10-21 15:07:36 +0530

    feat: Add navbar and landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+"use client"
+import { CustomButton } from "@/components/CustomButton";
+import cookingImage from "../../public/assets/cooking.jpg"
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router=useRouter()
   return (
     <>
-      Initial setup
+     {/* landing page design */}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+                {/* left container */}
+                <div className="w-full p-10 flex justify-center items-center ">
+                    <div className="block">
+                        <div className="font-bold text-4xl font-Rubik text-primary">
+                            Dive into delicious recipes
+                        </div>
+                        <div className="font-Rubik">
+                            If you are a chef, no matter how good a chef you are, it's not good cooking for yourself; the joy is in cooking for others.
+                        </div>
+                        <div className="flex gap-2">
+                        <CustomButton text="Sign UP" eventFunction={()=>{router.push('/auth/signup')}}/>
+                        <CustomButton text="Go to recipes" eventFunction={()=>{router.push('/recipes')}}/>
+                        </div>
+                    </div>
+                </div>
+                {/* right container */}
+                <div className="w-full">
+                    <img src={cookingImage.src} alt="cooking" />
+                </div>
+            </div>
     </>
   );
 }
